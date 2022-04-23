00ee5dc158d81abaa13be64e5e67ba61e0997b6e
Jude Morrissey <judemorrissey@users.noreply.github.com>
2022-04-22 18:09:44 -0700

    calculator sema work in progress

--- src/screens/SemaHome/SemaHome.tsx
+++ src/screens/SemaHome/SemaHome.tsx
@@ -3,13 +3,47 @@ import {Text, View} from 'react-native';
 
 import styles from './styles';
 
+import Button from 'components/Button';
+
 type Props = {};
+
+const outputText = '1234';
 
 function SemaHome(props: Props) {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{`Hello Friends`}</Text>
-      <Text style={styles.text}>{`I'm new`}</Text>
+      <View style={styles.outputContainer}>
+        <Text style={styles.text}>{outputText}</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonsRowContainer}>
+          <Button style={styles.button} title="/" />
+        </View>
+        <View style={styles.buttonsRowContainer}>
+          <Button style={styles.button} title="7" />
+          <Button style={styles.button} title="8" />
+          <Button style={styles.button} title="9" />
+          <Button style={styles.button} title="x" />
+        </View>
+        <View style={styles.buttonsRowContainer}>
+          <Button style={styles.button} title="4" />
+          <Button style={styles.button} title="5" />
+          <Button style={styles.button} title="6" />
+          <Button style={styles.button} title="-" />
+        </View>
+        <View style={styles.buttonsRowContainer}>
+          <Button style={styles.button} title="1" />
+          <Button style={styles.button} title="2" />
+          <Button style={styles.button} title="3" />
+          <Button style={styles.button} title="+" />
+        </View>
+        <View style={styles.buttonsRowContainer}>
+          <Button style={styles.button} title="0" />
+          <Button style={styles.button} title="00" />
+          <Button style={styles.button} title="." />
+          <Button style={styles.button} title="=" />
+        </View>
+      </View>
     </View>
   );
 }
--- src/screens/SemaHome/styles.ts
+++ src/screens/SemaHome/styles.ts
@@ -5,15 +5,40 @@ import {StyleSheet} from 'react-native';
 import theme from 'lib/theme';
 
 type Styles = {
+  button: ViewStyle;
+  buttonsContainer: ViewStyle;
+  buttonsRowContainer: ViewStyle;
   container: ViewStyle;
+  outputContainer: ViewStyle;
   text: TextStyle;
 };
 
 export default StyleSheet.create<Styles>({
-  container: {
+  button: {
+    flex: 1,
+    marginHorizontal: 4,
+  },
+  buttonsContainer: {
     flex: 1,
   },
+  buttonsRowContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: 8,
+  },
+  container: {
+    backgroundColor: 'steelblue',
+    flex: 1,
+    padding: 8,
+  },
+  outputContainer: {
+    alignItems: 'flex-end',
+    backgroundColor: '#6195ED',
+    padding: 16,
+  },
   text: {
-    color: theme.colors.text.importance.veryHigh,
+    color: theme.colors.text.importance.veryHighDark,
+    fontSize: 24,
   },
 });
