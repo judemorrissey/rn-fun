5a1661ab6e194eb97c9390e3d30daf299512f2c6
Jude Morrissey <judemorrissey@users.noreply.github.com>
2022-04-25 22:55:26 -0700

    add boolean tests

--- src/lib/Grid/test.ts
+++ src/lib/Grid/test.ts
@@ -12,6 +12,10 @@ const getRandomString = () => {
     .replace(/\//gu, ' ');
 };
 
+const getRandomBool = () => {
+  return Math.random() > 0.5;
+};
+
 describe('Grid', () => {
   describe('<number>', () => {
     let height: number;
@@ -201,6 +205,104 @@ describe('Grid', () => {
     describe('.fill', () => {
       it('fills the entire grid with an arbitrary value', () => {
         const newValue = getRandomString();
+        grid.fill(newValue);
+        expect([...grid].every((value) => value === newValue)).toStrictEqual(
+          true,
+        );
+      });
+    });
+  });
+
+  describe('<boolean>', () => {
+    let height: number;
+    let width: number;
+    let initialValue: boolean;
+    let grid: Grid<boolean>;
+
+    beforeEach(() => {
+      height = getRandomInt(false);
+      width = getRandomInt(false);
+      initialValue = getRandomBool();
+      grid = new Grid(width, height, initialValue);
+    });
+
+    it('gets filled with an initial value', () => {
+      expect([...grid].every((value) => value === initialValue)).toStrictEqual(
+        true,
+      );
+    });
+
+    it('has getters for basic dimensions', () => {
+      expect(grid.getHeight()).toStrictEqual(height);
+      expect(grid.getWidth()).toStrictEqual(width);
+    });
+
+    describe('.get and .set', () => {
+      it('gets and sets data in the grid', () => {
+        const x = Math.min(getRandomInt(true), width - 1);
+        const y = Math.min(getRandomInt(true), height - 1);
+        const newValue = getRandomBool();
+        expect(grid.get(x, y)).toStrictEqual(initialValue);
+        expect(grid.set(x, y, newValue)).toStrictEqual(true);
+        expect(grid.get(x, y)).toStrictEqual(newValue);
+        expect(grid.set(x, y, initialValue)).toStrictEqual(true);
+        expect(grid.get(x, y)).toStrictEqual(initialValue);
+      });
+    });
+
+    describe('.getColumn', () => {
+      it('gets a column from the grid', () => {
+        const x = Math.min(getRandomInt(true), width - 1);
+        let column = grid.getColumn(x);
+        expect(column?.length).toStrictEqual(height);
+        let newValue: boolean;
+        do {
+          newValue = getRandomBool();
+        } while (newValue === initialValue);
+        for (let i = 0; i < height; ++i) {
+          grid.set(x, i, newValue);
+        }
+        column = grid.getColumn(x);
+        expect(column?.every((value) => value === newValue)).toStrictEqual(
+          true,
+        );
+        let countNewValue = 0;
+        for (const value of grid) {
+          if (value === newValue) {
+            ++countNewValue;
+          }
+        }
+        expect(countNewValue).toStrictEqual(height);
+      });
+    });
+
+    describe('.getRow', () => {
+      it('gets a row from the grid', () => {
+        const y = Math.min(getRandomInt(true), height - 1);
+        let row = grid.getRow(y);
+        expect(row?.length).toStrictEqual(width);
+        let newValue: boolean;
+        do {
+          newValue = getRandomBool();
+        } while (newValue === initialValue);
+        for (let i = 0; i < width; ++i) {
+          grid.set(i, y, newValue);
+        }
+        row = grid.getRow(y);
+        expect(row?.every((value) => value === newValue)).toStrictEqual(true);
+        let countNewValue = 0;
+        for (const value of grid) {
+          if (value === newValue) {
+            ++countNewValue;
+          }
+        }
+        expect(countNewValue).toStrictEqual(width);
+      });
+    });
+
+    describe('.fill', () => {
+      it('fills the entire grid with an arbitrary value', () => {
+        const newValue = getRandomBool();
         grid.fill(newValue);
         expect([...grid].every((value) => value === newValue)).toStrictEqual(
           true,
