a95b9b054b688d344f13e59fb86d1f1dbabf5600
Jude Morrissey <judemorrissey@users.noreply.github.com>
2022-04-29 16:12:11 -0700

    introduce Fonts style file + add more colors (#30)
    * introduce Fonts style file, add more colors
    
    * add `as const` to fonts

--- src/lib/theme/Colors.ts
+++ src/lib/theme/Colors.ts
@@ -1,11 +1,26 @@
+// super useful color sourcing tool: https://chir.ag/projects/name-that-color
 const palette = {
+  // shades of gray
+  blueGray: '#434C59',
+  gray: '#808080',
+  greenGray: '#465945',
+
+  // darker shades
+  darkBlue: '#384555',
+  darkGreen: '#3F583B',
+
+  // other colors
+  black: '#000000',
   purple: '#660099',
   white: '#FFFFFF',
-};
+} as const;
 
 export default {
   backgrounds: {
     button: palette.purple,
+    characterTile: {
+      default: palette.blueGray,
+    },
   },
 
   text: {
