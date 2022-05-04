89f0612dcb51995cb7bfa2775e6f92c1dbf0026c
Jude Morrissey <judemorrissey@users.noreply.github.com>
2022-05-04 00:18:26 -0700

    minor typing change, use 👉 for evolution chain

--- src/screens/GraphqlExample/GraphqlExample.tsx
+++ src/screens/GraphqlExample/GraphqlExample.tsx
@@ -1,5 +1,5 @@
 import type {ListRenderItem} from 'react-native';
-import type {PokemonQuery} from './gql';
+import type {Pokemon, PokemonQuery} from './gql';
 
 import * as React from 'react';
 import {useCallback} from 'react';
@@ -20,30 +20,29 @@ function ItemSeparator() {
 export function GraphqlExample(props: Props) {
   const {data, loading} = useQuery<PokemonQuery>(ALL_POKEMON_QUERY);
 
-  const keyExtractor = useCallback((item, index) => {
+  const keyExtractor = useCallback((item: Pokemon) => {
     return String(item.id);
   }, []);
 
-  const renderItem: ListRenderItem<PokemonQuery['all_pokemon'][number]> =
-    useCallback(({item}) => {
-      return (
-        <View style={styles.itemContainer}>
-          <Text style={styles.heading}>{utils.capitalize(item.name)}</Text>
-          <Text>
-            <Text style={styles.label}>{`National Dex #: `}</Text>
-            <Text style={styles.text}>{item.id}</Text>
+  const renderItem: ListRenderItem<Pokemon> = useCallback(({item}) => {
+    return (
+      <View style={styles.itemContainer}>
+        <Text style={styles.heading}>{utils.capitalize(item.name)}</Text>
+        <Text>
+          <Text style={styles.label}>{`National Dex #: `}</Text>
+          <Text style={styles.text}>{item.id}</Text>
+        </Text>
+        <Text>
+          <Text style={styles.label}>{`Evolution Chain: `}</Text>
+          <Text style={styles.text}>
+            {item.pokemon_v2_evolutionchain.pokemon_v2_pokemonspecies
+              .map((pokemon) => utils.capitalize(pokemon.name))
+              .join(' 👉 ')}
           </Text>
-          <Text>
-            <Text style={styles.label}>{`Evolution Chain: `}</Text>
-            <Text style={styles.text}>
-              {item.pokemon_v2_evolutionchain.pokemon_v2_pokemonspecies
-                .map((pokemon) => utils.capitalize(pokemon.name))
-                .join(' -> ')}
-            </Text>
-          </Text>
-        </View>
-      );
-    }, []);
+        </Text>
+      </View>
+    );
+  }, []);
 
   return (
     <View style={styles.container}>
