1d491843a543f43fc75c120326fffb38f725b569
Jude Morrissey <judemorrissey@users.noreply.github.com>
2022-05-03 18:30:24 -0700

    introduce flipping animation + cleanup of code to be more react-y

--- src/screens/BlaireHome/_private/SingleCard/SingleCard.tsx
+++ src/screens/BlaireHome/_private/SingleCard/SingleCard.tsx
@@ -1,7 +1,15 @@
-import * as React from 'react';
-import {useCallback} from 'react';
 import type {ImageSourcePropType} from 'react-native';
-import {Dimensions, Image, TouchableOpacity, View} from 'react-native';
+
+import * as React from 'react';
+import {useCallback, useEffect, useState} from 'react';
+import {Dimensions, Image, Pressable, View} from 'react-native';
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+
 import styles from './styles';
 
 const coverImage = require('screens/BlaireHome/assets/cover.png');
@@ -18,30 +26,60 @@ type Props = {
 function SingleCard(props: Props) {
   const {cardImageSource, onPress, flipped} = props;
 
+  const [isInitialized, setIsInitialized] = useState<boolean>(false);
+  const [internalFlipped, setInternalFlipped] = useState<boolean>(flipped);
+
+  const rotation = useSharedValue(0);
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotateY: `${rotation.value}deg`,
+        },
+      ],
+    };
+  });
+
+  useEffect(() => {
+    if (isInitialized) {
+      rotation.value =
+        // "hide" the card's back
+        withTiming(
+          90,
+          {
+            duration: 500,
+          },
+          () => {
+            // swap image here?
+            runOnJS(setInternalFlipped)(flipped);
+
+            // now "show" the card front
+            rotation.value = withTiming(0, {
+              duration: 500,
+            });
+          },
+        );
+    } else {
+      setIsInitialized(true);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [flipped, rotation]);
+
   const handleClick = useCallback(() => {
     onPress();
   }, [onPress]);
 
   return (
     <View>
-      <View style={[styles.card, {height: WIDTH / numCols}]}>
-        <View>
+      <Animated.View
+        style={[styles.card, {height: WIDTH / numCols}, animatedStyle]}>
+        <Pressable onPress={handleClick}>
           <Image
-            source={cardImageSource}
-            style={
-              flipped
-                ? [styles.image, styles.flipped]
-                : [styles.image, styles.unflipped]
-            }
+            source={internalFlipped ? cardImageSource : coverImage}
+            style={styles.image}
           />
-          <TouchableOpacity onPress={handleClick}>
-            <Image
-              source={coverImage}
-              style={flipped ? styles.unflipped : styles.image}
-            />
-          </TouchableOpacity>
-        </View>
-      </View>
+        </Pressable>
+      </Animated.View>
     </View>
   );
 }
